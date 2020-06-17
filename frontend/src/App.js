3294238f658deca32e42706c4784691dfb17d049
anand-shukla-au5 <shuklaanand800@gmail.com>
2020-06-17 01:46:54 -0700

    nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import "./styles/App.css";
 // import Navbar from "./components/navbar/navbar";
+import Land from "./components/home/landing"
 import Login from "./components/login/login";
 import Register from "./components/register/register";
 import "@fortawesome/fontawesome-free/css/all.min.css";
@@ -12,12 +13,17 @@ function App() {
   return (
     <BrowserRouter>
       <div>
-        <Route path='/login'>
+        <Route path='/'>
+          <Land />
+        </Route>
+      </div>
+      <div>
+        <Route path='/login' exact strict>
           <Login />
         </Route>
       </div>
       <div>
-        <Route path='/register'>
+        <Route path='/register' exact strict>
           <Register />
         </Route>
       </div>
---
+++ frontend/src/components/home/landing.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../styles/landing.css'
+class Land extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Landing PAGE</h1>
+            </div>
+        )
+    }
+}
+export default Land
